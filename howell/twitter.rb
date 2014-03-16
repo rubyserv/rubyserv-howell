6ba934955d04884ab48ccf826572efb9fb009cc0
James Newton <james@Zaphyous.com>
2014-03-15 21:43:54 -0400

    start of the twitter plugin

--- howell/twitter.rb
+++ howell/twitter.rb
@@ -1,6 +1,6 @@
 module Howell
   match(/(twitter|tw) ([\w#\$]+)( \w+)?/) do |m, _, command, nth|
-    return unless RubyServ.config.respond_to?(:twitter)
+    return unless RubyServ.config.respond_to?(:api_keys) && RubyServ.config.api_keys.respond_to?(:twitter)
 
     nth = nth.nil? ? 0 : (nth.to_i > 20 ? 0 : nth.to_i)
 
@@ -10,15 +10,15 @@ module Howell
                get_last_tweet(command, nth)
              end
 
-    m.reply "\x02Twitter\x02: #{output}"
+    m.reply output
   end
 
   match(/https?:\/\/twitter\.com\/(#!\/)?[_0-9a-zA-Z]+\/status\/(\d+)/, skip_prefix: true) do |m, _, id|
-    m.reply "\x02Twitter\x02: #{get_last_tweet(id, 0)}"
+    m.reply get_last_tweet(id, 0)
   end
 
   match(/https?:\/\/twitter\.com\/(#!\/)?([_0-9a-zA-Z]+)/, skip_prefix: true) do |m, _, username|
-    m.reply "\x02Twitter\x02: #{get_last_tweet(username, 0)}"
+    m.reply get_last_tweet(username, 0)
   end
 
   def get_last_tweet(user, nth)
@@ -30,11 +30,19 @@ module Howell
                 twitter_request(:get, "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=#{user}")[nth]
               end
 
-    "\x0307#{request['text'].sub("\n", '-')}\x0307 - \x0303@#{request['user']['screen_name']}\x03 \x0302(#{request['user']['name']} - #{request['user']['description']}) - https://twitter.com/#{request['user']['screen_name']}\x03"
+    output  = "\x02Twitter\x02: \x0307\"#{request['text'].sub("\n", '-')}\"\x03 - \x0303@#{request['user']['screen_name']}\x03"
+    output += " \x0302(#{request['user']['name']}"
+    output += " - #{request['user']['description']}" unless request['user']['description'].nil?
+    output += ")\x03 -"
+    output += " \x0307followers:\x03 \x0302#{request['user']['followers_count']}\x03"
+    output += " \x0307following:\x03 \x0302#{request['user']['friends_count']}\x03"
+    output += " - #{request['user']['url']}" unless request['user']['url'].nil?
+    output += " - https://twitter.com/#{request['user']['screen_name']}"
+    output
   end
 
   def search_for_tag(tag, nth)
-    twitter_request(:get, "https://api.twitter.com/1.1/search/tweets.json?q=#{URI.escape(tag)}")['statuses']
+    #twitter_request(:get, "https://api.twitter.com/1.1/search/tweets.json?q=#{URI.escape(tag)}")['statuses']
   end
 
   def twitter_request(type, uri)
@@ -42,7 +50,7 @@ module Howell
   end
 
   def prepare_twitter_access_token
-    config = RubyServ.config.twitter
+    config = RubyServ.config.api_keys.twitter
     access = OAuth::Consumer.new(config.api_key, config.api_secret, {
       site: 'http://api.twitter.com',
       scheme: :header
