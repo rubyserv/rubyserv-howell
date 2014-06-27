68fe92735c92b474915d8fe71f1ece443c8dcb3d
James Newton <james@Zaphyous.com>
2014-06-27 15:13:05 -0400

    fix unexpected return problem

--- howell/linkinfo.rb
+++ howell/linkinfo.rb
@@ -7,12 +7,12 @@ module Howell
   ]
 
   match(%r{(https?:\/\/.*?)(?:\s|$|,|\.\s|\.$)}, skip_prefix: true) do |m, url|
-    return if URL_BLACKLIST.any? { |bl| url =~ bl }
+    unless URL_BLACKLIST.any? { |bl| url =~ bl }
+      html = Nokogiri::HTML(HTTParty.get(url))
 
-    html = Nokogiri::HTML(HTTParty.get(url))
-
-    if node = html.at_xpath('html/head/title')
-      m.reply node.blank? ? 'Link has no title' : node.text.strip
+      if node = html.at_xpath('html/head/title')
+        m.reply node.blank? ? 'Link has no title' : node.text.strip
+      end
     end
   end
 end
